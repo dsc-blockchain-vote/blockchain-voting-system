533f953e86be56be70b0efbd0a6d45537a329c33
unknown <henryhengyi.lin@mail.utoronto.ca>
2021-02-11 20:08:16 -0800

    removed unnecessary require statements

--- smart-contracts/Election.sol
+++ smart-contracts/Election.sol
@@ -8,19 +8,19 @@ import {DataTypes} from "./DataTypes.sol";
 @title Election
 @dev Main contract used to set up and participate in the election on the blockchain
  */
-contract Election { 
+contract Election {
     DataTypes.Candidate[] public candidates;
-    
+
     enum votingTypes {firstPastThePost}
     votingTypes votingType;
 
-    uint public endTime;
-    uint public startTime;
+    uint256 public endTime;
+    uint256 public startTime;
 
     address public organizer;
 
     mapping(address => DataTypes.Voter) public voters;
-    
+
     /**
     @dev checks whether the sender of the request is the organizer or not
      */
@@ -28,56 +28,55 @@ contract Election {
         require(msg.sender == organizer);
         _;
     }
-    
+
     /**
     @dev checks whether the sender made the request within the given endtime
      */
     modifier onlyWhileOpen() {
-        require(block.timestamp < endTime );
+        require(block.timestamp < endTime);
         require(block.timestamp >= startTime);
         _;
     }
-    
+
     /**
     @dev initilazes the Election by storing the candidates and start, end time
     @param candidateNames array of strings containing the candidate names
     @param endTime_ uint storing the end time in epoch seconds
     @param startTime_ uint storing the start time in epoch seconds
      */
-    constructor(string[] memory candidateNames, uint endTime_, uint startTime_) {
+    constructor(
+        string[] memory candidateNames,
+        uint256 endTime_,
+        uint256 startTime_
+    ) {
         votingType = votingTypes.firstPastThePost;
         organizer = msg.sender;
         endTime = endTime_;
         startTime = startTime_;
-        for (uint i = 0; i < candidateNames.length; i++) {
-            candidates.push(DataTypes.Candidate({
-                name: candidateNames[i],
-                voteCount: 0,
-                id: i
-            }));
+        for (uint256 i = 0; i < candidateNames.length; i++) {
+            candidates.push(
+                DataTypes.Candidate({
+                    name: candidateNames[i],
+                    voteCount: 0,
+                    id: i
+                })
+            );
         }
     }
-    
+
     /**
     @dev allows the organizer to give voting privilege to a voter
     @param voterAddress address of the voter account
      */
     function giveRightToVote(address voterAddress) public onlyHost {
-        // Do we need this here??? (the 2 requires)
-        require(
-            !voters[voterAddress].voted,
-            "The voter already voted."
-        );
-        require(!voters[voterAddress].validVoter);
         voters[voterAddress].validVoter = true;
     }
-
 
     /**
     @dev allow valid voter to caste their votes
     @param candidateID unint representing the candidate who got the vote
      */
-    function vote(uint candidateID) public onlyWhileOpen {
+    function vote(uint256 candidateID) public onlyWhileOpen {
         DataTypes.Voter storage sender = voters[msg.sender];
         require(sender.validVoter, "Has no right to vote");
         require(!sender.voted, "Already voted.");
@@ -90,8 +89,7 @@ contract Election {
     @dev calculate the winning candidate according to the choosen voting system
     @return winningCandidate_ the id of the winner candidate
      */
-    function winningCandidate() public returns (uint winningCandidate_)
-    {
+    function winningCandidate() public returns (uint256 winningCandidate_) {
         FirstPastThePost countMethod = new FirstPastThePost(candidates);
         winningCandidate_ = countMethod.calculate();
     }
@@ -99,8 +97,7 @@ contract Election {
     /**
     @dev returns the winning candidate name
      */
-    function winnerName() public returns (string memory winnerName_)
-    {
+    function winnerName() public returns (string memory winnerName_) {
         winnerName_ = candidates[winningCandidate()].name;
     }
 }
