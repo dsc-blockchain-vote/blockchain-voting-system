1033e9e3eb1745c6594a4ddf6170f0ff73d1530b
unknown <henryhengyi.lin@mail.utoronto.ca>
2021-02-13 23:19:30 -0800

    Updated files to be up to date with contracts branch and updated calculate() in FirstPastThePost.sol and getWinner() in VotingSystem.sol to handle cases where there are ties in elections.

--- smart-contracts/FirstPastThePost.sol
+++ smart-contracts/FirstPastThePost.sol
@@ -1,37 +1,46 @@
 pragma solidity >=0.7.0 <0.8.0;
 pragma experimental ABIEncoderV2;
 import "./VotingSystem.sol";
-import {DataTypes} from "./DataTypes.sol";
+import "./DataTypes.sol";
 
 /**
 @title First Past the Post
 @dev Implimentation of the VotingSystem interface
  */
 contract FirstPastThePost is VotingSystem {
-    
     /**
     @dev initializes FirstPastThePost contract by passing in an array of DataTypes.Candidate
-    @param candidate_ array of DataTypes.Candidate struct
+    @param _candidate array of DataTypes.Candidate struct
      */
-    constructor(DataTypes.Candidate[] memory candidate_) {
-        candidates = candidate_;
-        
+    constructor(DataTypes.Candidate[] memory _candidate) {
+        for (uint256 i = 0; i < _candidate.length; i++) {
+            candidates.push(_candidate[i]);
+        }
     }
-    
+
     /**
     @dev Calculates the winner under First Past the Post system
-    @return winningCandidate_ It is the id of the winning candidate
+    @return winningCandidate It is the id of the winning candidate
      */
-    function calculate() external view override returns (uint winningCandidate_)
+    function calculate()
+        external
+        view
+        override
+        returns (uint256[] memory winningCandidate)
     {
-        uint winningVoteCount = 0;
-        for (uint i = 0; i < candidates.length; i++) {
+        uint256 winningVoteCount = 0;
+        uint256 tempIndex = 0;
+        for (uint256 i = 0; i < candidates.length; i++) {
             if (candidates[i].voteCount > winningVoteCount) {
                 winningVoteCount = candidates[i].voteCount;
-                winningCandidate_ = i;
+                delete winningCandidate;
+                tempIndex = 0;
+                winningCandidate[tempIndex] = i;
+            }
+            if (candidates[i].voteCount == winningVoteCount) {
+                tempIndex++;
+                winningCandidate[tempIndex] = i;
             }
         }
     }
-    
-    
 }
--- smart-contracts/VotingSystem.sol
+++ smart-contracts/VotingSystem.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.7.0 <0.8.0;
 pragma experimental ABIEncoderV2;
 
-import {DataTypes} from "./DataTypes.sol";
+import "./DataTypes.sol";
 
 /**
 @title Voting System
@@ -9,15 +9,28 @@ import {DataTypes} from "./DataTypes.sol";
  */
 abstract contract VotingSystem {
     DataTypes.Candidate[] candidates;
-    
-    function calculate() external view virtual returns(uint);
-    
+
+    function calculate() external view virtual returns (uint256[] memory);
+
     /**
     @dev Returns the name of the winning candidate
      */
-    function getWinner() external view returns (string memory winnerName_)
-    {
-        winnerName_ = candidates[this.calculate()].name;
+    function getWinner() external view returns (string memory _winnerName) {
+        uint256[] memory winners = this.calculate();
+        if (winners.length > 1) {
+            _winnerName = "tie: ";
+            for (uint256 i = 0; i < winners.length; i++) {
+                // abi.encodePacked(arg) is an ABI encoding function that concatinates 2 strings together;
+                _winnerName = string(
+                    abi.encodePacked(
+                        _winnerName,
+                        candidates[winners[i]].name,
+                        " "
+                    )
+                );
+            }
+        } else {
+            _winnerName = candidates[winners[0]].name;
+        }
     }
 }
-    
