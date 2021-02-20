c13c675e0b83079f9ac06337e76dfee0e3ad5ea9
Akshit Goyal <goyalakshit.ag@gmail.com>
2021-02-20 16:17:07 -0500

    express server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const express = require('express');
+
+const env = process.env.NODE_ENV;
+
+// start the express server
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
