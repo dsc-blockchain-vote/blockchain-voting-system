06383bd879e24156770de92a82e6815db7091908
Akshit Goyal <goyalakshit.ag@gmail.com>
2021-02-22 16:26:42 -0500

    Revert "Revert "Smart Contracts created""

---
+++ smart-contracts/DataTypes.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.7.0 <0.8.0;
+pragma experimental ABIEncoderV2;
+
+/**
+@dev library to have common datatypes across all contracts
+ */
+library DataTypes{
+    /// Voter datatype. Stores whether they have voted, whom they have voted for and
+    /// whether they are allowed to vote in the election
+    struct Voter {
+        bool voted;  
+        uint votedFor; 
+        bool validVoter;
+    }
+
+    /// election candidate datatype. Stores the name, votes received and id
+    struct Candidate {
+        string name;
+        uint voteCount;
+        uint id;
+    }    
+}
