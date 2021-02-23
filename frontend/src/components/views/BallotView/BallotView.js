a1be031f6ce7857f1b35127eec96be5d20aeca22
celery <ceIery@users.noreply.github.com>
2021-02-23 04:19:39 -0500

    [BV-34] Fixed issue where selected candidate was not shown

--- frontend/src/components/views/BallotView/BallotView.js
+++ frontend/src/components/views/BallotView/BallotView.js
@@ -16,9 +16,9 @@ class BallotView extends Component {
     state = {
         title: "Election 1",
         candidates: [
-            { id: 1, name: "Name 1" },
-            { id: 2, name: "Name 2" },
-            { id: 3, name: "Name 3" },
+            { id: "1", name: "Name 1" },
+            { id: "2", name: "Name 2" },
+            { id: "3", name: "Name 3" },
         ],
         otherEnabled: true,
     };
