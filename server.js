65c268d868a1dbaa0cef3aec076f35b5aab0f375
Shubh Bapna <shubh.bapna@mail.utoronto.ca>
2021-03-17 05:20:50 +0530

    api getting integrated with firebase

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 "use strict";
 require("dotenv").config();
 const express = require("express");
+const csrf = require("csrf");
 const Web3 = require("web3");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
+const admin = require("firebase-admin");
+const serviceAccount = require("./service-account.json");
 const { abi, bytecode } = require("./compile");
+const db = admin.database();
+
+// initialize firebase
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://blockchain-try1-default-rtdb.firebaseio.com",
+});
 
 // load required environment variables
 const env = process.env.NODE_ENV;
@@ -43,29 +53,55 @@ const voterData = async (voterAccount, address) => {
   return result;
 };
 
-//middleware
-const OrganizerContract = (req, res, next) => {
-  const organizerAccount = req.body.organizerAccount;
-  try {
-    if (electionStorage.hasOwnProperty(organizerAccount)) {
-      return next();
-    } else {
-      res.status(401).send("Election contract does not exist");
-      return;
-    }
-  } catch (error) {
-    res.status(501).send("Server error");
-  }
+//middlewares
+
+// verify session tokens to protect against cross site forgery
+const csrfMiddleWare = csrf({ cookie: true });
+app.all("*", (req, res, next) => {
+  res.cookie("XSRF-TOKEN", req.csrfToken);
+  next();
+});
+
+// verify the user has logged in
+const verifyUser = (res, req, next) => {
+  const sessionCookie = req.cookies.session || "";
+  admin
+    .auth()
+    .verifySessionCookie(sessionCookie, true)
+    .then((decodedClaims) => {
+      if (decodedClaims.isOrganizer === true) {
+        req.isOrganizer = true;
+      } else {
+        req.isOrganizer = false;
+      }
+      req.userID = decodedClaims.uid;
+      next();
+    })
+    .catch((error) => {
+      res.redirect("/login");
+    });
 };
 
 // if the voter has voted then return the candidate id. If not then return false
-// TODO verify voter has logged in
-app.get("/voter/election/:electionID/", async (req, res) => {
-  const { voterAccount } = req.body;
+app.get("/api/election/:electionID", verifyUser, async (req, res) => {
+  if (req.isOrganizer){
+    res.status(400).send("bad request");
+    return;
+  }
+  const { userID } = req.body;
   try {
+    const userRef = db.ref("users/" + userID);
+    let voterAccount = null;
+    userRef.on("value", (snapshot) => {
+      voterAccount = snapshot.val().account;
+    });
+    if(voterAccount === null){
+      res.status(400).send("bad request");
+      return;
+    }
     const result = await voterData(
       voterAccount,
-      electionAddress[req.params.electionID]
+      req.params.electionID
     );
     const response = { result: false };
     if (result.voted && result.validVoter) {
@@ -74,13 +110,12 @@ app.get("/voter/election/:electionID/", async (req, res) => {
     res.send(response);
   } catch (error) {
     console.log(error);
-    res.status(401).send("Bad request");
+    res.status(400).send("Bad request");
   }
 });
 
 // check voter eligibility
-// TODO ensure that the voter is logged in
-app.get("/voter/election/:electionID/verify", async (req, res) => {
+app.get("/voter/election/:electionID/verify", verifyUser, async (req, res) => {
   const { voterAccount } = req.body;
   try {
     const result = await voterData(
@@ -96,10 +131,27 @@ app.get("/voter/election/:electionID/verify", async (req, res) => {
 });
 
 // casting vote
-// TODO verify the voter has logged in
-app.post("/voter/election/:electionID/vote", async (req, res) => {
-  const { voterAccount, candidateID } = req.body;
+app.post("/api/election/:electionID/vote", verifyUser, async (req, res) => {
+  if (req.isOrganizer){
+    res.status(400).send("bad request");
+    return;
+  }
+  const { candidateID, userID } = req.body;
   try {
+    const voterRef = db.ref("users/" + userID);
+    let voterAccount = null;
+    voterRef.on("value", (snapshot) => {
+      voterAccount = snapshot.val().account;
+    });
+    const electionRef = db.ref("elections/" + req.params.electionID);
+    let electionAddress = null;
+    electionRef.on("value", (snapshot) => {
+      electionAddress = snapshot.val().address;
+    });
+    if (voterAccount === null || electionAddress === null){
+      res.status(400).send("bad request");
+      return;
+    }
     const provider = new HDWalletProvider({
       mnemonic: mnemonic,
       providerOrUrl: URL,
@@ -109,7 +161,7 @@ app.post("/voter/election/:electionID/vote", async (req, res) => {
     const web3 = new Web3(provider);
     const contract = await new web3.eth.Contract(
       abi,
-      electionAddress[req.params.electionID]
+      electionAddress
     );
     await contract.methods
       .vote(candidateID)
@@ -126,57 +178,45 @@ app.post("/voter/election/:electionID/vote", async (req, res) => {
     } else if (msg.includes("Already voted")) {
       response = "Vote has already been cast";
     }
-    res.status(401).send(response);
+    res.status(400).send(response);
   }
 });
 
-// add eligible voters
-// TODO ensure that organizer is logged in
-app.post(
-  "/organizer/election/:electionID/validate",
-  OrganizerContract,
-  async (req, res) => {
-    const { organizerAccount, voterAccounts } = req.body;
-    try {
-      const provider = new HDWalletProvider({
-        mnemonic: mnemonic,
-        providerOrUrl: URL,
-        addressIndex: organizerAccount,
-        numberOfAddresses: 1,
-      });
-      const web3 = new Web3(provider);
-      const contract = await new web3.eth.Contract(
-        abi,
-        electionAddress[req.params.electionID]
-      );
-      const result = {};
-      for (let address of voterAccounts) {
-        let voterProvider = new HDWalletProvider({
-          mnemonic: mnemonic,
-          providerOrUrl: URL,
-          addressIndex: address,
-          numberOfAddresses: 1,
-        });
-        await contract.methods
-          .giveRightToVote(voterProvider.getAddress(0))
-          .send({ from: provider.getAddress(0) })
-          .then((result[address] = true));
-        voterProvider.engine.stop();
-      }
-      provider.engine.stop();
-      res.send(result);
-    } catch (error) {
-      console.log(error);
-      res.status(401).send("Bad request");
-    }
-  }
-);
-
 // starting election endpoint
-// TODO middleware to verify organizer account
-app.post("/organizer/election/start", async (req, res) => {
-  const { organizerAccount, candidates, endTime, startTime } = req.body;
+app.post("/api/election/start", verifyUser, async (req, res) => {
+  if (!req.isOrganizer) {
+    res.status(400).send("bad request");
+    return;
+  }
+  const { userID, electionID } = req.body;
   try {
+    const userRef = db.ref("users/" + userID);
+    let organizerAccount = null;
+    userRef.on("value", (snapshot) => {
+      organizerAccount = snapshot.val().organizerAccount;
+    });
+    const electionRef = db.ref("elections/" + electionID);
+    let electionData = {};
+    electionRef.on("value", (snapshot) => {
+      const data = snapshot.val();
+      if (
+        data.organizerID === userID &&
+        snapshot.child("candidates").exists() &&
+        snapshot.child("endTime").exists() &&
+        snapshot.child("startTime").exists() &&
+        snapshot.child("validVoters").exists() &&
+        !snapshot.child("address").exists()
+      ) {
+        electionData.candidates = data.candidates;
+        electionData.endTime = data.endTime;
+        electionData.startTime = data.startTime;
+        electionData.validVoters = data.validVoters;
+      }
+    });
+    if (organizerAccount === null || electionData === {}) {
+      res.status(400).send("Bad request");
+      return;
+    }
     const provider = new HDWalletProvider({
       mnemonic: mnemonic,
       providerOrUrl: URL,
@@ -188,18 +228,36 @@ app.post("/organizer/election/start", async (req, res) => {
     const deployTx = await contract
       .deploy({
         data: "0x" + bytecode,
-        arguments: [candidates, endTime, startTime],
+        arguments: [
+          electionData.candidates,
+          electionData.endTime,
+          electionData.startTime,
+        ],
       })
       .send({ from: provider.getAddress(0), gas: 3000000 });
 
+    electionRef.update({
+      address: deployTx.options.address,
+    });
+
     console.log("Deployed election at " + deployTx.options.address);
-    electionAddress.push(deployTx.options.address);
-    if (!electionStorage.hasOwnProperty(organizerAccount)) {
-      electionStorage[organizerAccount] = [];
+
+    for (let id in electionData.validVoter) {
+      let voterRef = db.ref("users/" + id);
+      voterRef.on("value", (snapshot) => {
+        const voterAccount = snapshot.value().account;
+        let voterProvider = new HDWalletProvider({
+          mnemonic: mnemonic,
+          providerOrUrl: URL,
+          addressIndex: voterAccount,
+          numberOfAddresses: 1,
+        });
+        await contract.methods
+          .giveRightToVote(voterProvider.getAddress(0))
+          .send({ from: provider.getAddress(0) })
+        voterProvider.engine.stop();
+      });
     }
-    electionStorage[organizerAccount].push(
-      electionAddress.indexOf(deployTx.options.address)
-    );
 
     provider.engine.stop();
 
@@ -211,7 +269,57 @@ app.post("/organizer/election/start", async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-    res.status(400).send("Invalid request");
+    res.status(400).send("bad request");
+  }
+});
+
+// login end point
+app.post("/api/login", async (req, res) => {
+  const idToken = req.body.idToken.toString();
+  const expiresIn = 30 * 60 * 1000; // session cookie expires in 30 minutes
+  admin
+    .auth()
+    .createSessionCookie(idToken, { expiresIn })
+    .then(
+      (sessionCookie) => {
+        const options = { maxAge: expiresIn, httpOnly: true };
+        res.cookie("session", sessionCookie, options);
+        res.end(JSON.stringify({ status: Success }));
+      },
+      (error) => {
+        res.status(401).send("unauthorized");
+      }
+    );
+});
+
+// logout endpoint
+app.get("/api/logout", (req, res) => {
+  res.clearCookie("session");
+  res.redirect("/api/login");
+});
+
+// register endpoint
+app.post("/api/register", async (req, res) => {
+  try {
+    const { name, email, password, isOrganizer } = req.body;
+    const userRecord = await admin.auth().createUser({
+      email: email,
+      password: password,
+    });
+    await admin
+      .auth()
+      .setCustomUserClaims(userRecord.uid, { isOrganizer: isOrganizer });
+
+    // TODO blockchain account generation
+    const ref = db.ref("users");
+    const currUser = ref.child(userRecord.uid);
+    const newUser = { name: name, email: email, account: 0 };
+    await currUser.update(newUser);
+
+    res.status(200).send("User successfully registerd");
+    res.redirect("/api/login");
+  } catch (error) {
+    res.status(400).send("Bad request");
   }
 });
 
