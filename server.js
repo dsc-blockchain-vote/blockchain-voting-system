4fade96afb8973fa4147c41e43e4c8a18872142c
Shubh Bapna <shubh.bapna@mail.utoronto.ca>
2021-03-26 03:13:39 +0530

    login returns isOrganizer and name of user

--- server.js
+++ server.js
@@ -448,16 +448,20 @@ app.put("/api/election/:electionID/deploy", verifyUser, async (req, res) => {
 app.post("/api/login", async (req, res) => {
   const idToken = req.body.idToken.toString();
   const expiresIn = 59 * 60 * 1000; // session cookie expires in 59 minutes
-  let isOrganizer;
-  admin
+  let isOrganizer, uid;
+  await admin
     .auth()
     .verifyIdToken(idToken)
     .then((decodedToken) => {
+      uid = decodedToken.uid;
       isOrganizer = decodedToken.isOrganizer;
     })
     .catch((error) => {
       res.end("Unauthorized");
     });
+
+  const ref = db.ref("users/" + uid);
+  const name = await (await ref.once("value")).val().name;
   admin
     .auth()
     .createSessionCookie(idToken, { expiresIn })
@@ -465,7 +469,7 @@ app.post("/api/login", async (req, res) => {
       (sessionCookie) => {
         const options = { maxAge: expiresIn, httpOnly: true };
         res.cookie("session", sessionCookie, options);
-        res.end({ isOrganizer: isOrganizer });
+        res.send({ isOrganizer: isOrganizer, name: name });
       },
       (error) => {
         res.status(401);
@@ -494,7 +498,6 @@ app.post("/api/register", async (req, res) => {
     await admin
       .auth()
       .setCustomUserClaims(userRecord.uid, { isOrganizer: isOrganizer });
-
     const accRef = db.ref("accounts");
     let acc = 10;
     const snapshot = await accRef.once("value");
