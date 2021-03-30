56351574d73afeb97c408fe3fcdd927c1f3da19c
celery <ceIery@users.noreply.github.com>
2021-03-30 17:19:05 -0400

    Deploy button and results view

--- frontend/src/components/views/ResultsView/ResultsView.js
+++ frontend/src/components/views/ResultsView/ResultsView.js
@@ -1,85 +1,102 @@
-import React, { Component } from 'react';
-import Paper from '@material-ui/core/Paper';
-import Container from '@material-ui/core/Container';
+import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
 import {
-  Chart,
-  PieSeries,
-  BarSeries,
-  Legend,
-  Title,
-  Tooltip,
-  ArgumentAxis,
-  ValueAxis
-} from '@devexpress/dx-react-chart-material-ui';
-import { EventTracker, Animation } from '@devexpress/dx-react-chart';
-import axios from 'axios';
-import { CircularProgress } from '@material-ui/core';
-import { Redirect } from 'react-router';
+    Chart,
+    PieSeries,
+    BarSeries,
+    Legend,
+    Title,
+    Tooltip,
+    ArgumentAxis,
+    ValueAxis,
+} from "@devexpress/dx-react-chart-material-ui";
+import { EventTracker, Animation } from "@devexpress/dx-react-chart";
+import axios from "axios";
+import { CircularProgress } from "@material-ui/core";
+import { Redirect } from "react-router";
 
 const data = [
-    { name: "Candidate 1", votes: 1234 },
-    { name: "Candidate 2", votes: 643 },
-    { name: "Candidate 3", votes: 636 },
-    { candidate: "Candidate 4", votes: 320 },
-  ];
+    { candidate: "Shubh Bapna", votes: 4 },
+    { candidate: "Henry Lin", votes: 3 },
+    { candidate: "Chloe Lin", votes: 2 },
+    { candidate: "Shivangi Pathak", votes: 1 },
+];
+
+async function stall(stallTime = 3000) {
+    await new Promise((resolve) => setTimeout(resolve, stallTime));
+}
 
 class ResultsView extends Component {
-    state = {  }
+    state = {};
     constructor(props) {
         super(props);
-        this.state = {data: {}, loading: true};
+        this.state = { data: data, loading: true };
     }
 
     componentDidMount() {
         this.getData();
     }
 
-    getData() {
+    async getData() {
         axios
-            .get(`http://localhost:5000/api/election/${this.props.match.params.id}/result`, {
-                withCredentials: true,
-            })
-            .then((response) => {
+            .get(
+                `http://localhost:5000/api/election/${this.props.match.params.id}/result`,
+                {
+                    withCredentials: true,
+                }
+            )
+            .then(async (response) => {
                 console.log(response);
-                this.setState({data: response.data, loading: false});
+                this.setState({ loading: false });
             })
             .catch((error) => {
                 console.log(error);
-                this.setState({loading: false});
+                this.setState({ loading: false });
             });
     }
 
-    render() { 
+    render() {
         if (this.state.loading) {
-            return (<CircularProgress/>);
+            return (
+                <Container maxWidth="md">
+                    <CircularProgress />
+                </Container>
+            );
         }
         if (this.state.data === {}) {
-            return (<Redirect to="/elections"/>);
+            return <Redirect to="/elections" />;
         }
         return (
             <Container maxWidth="md">
                 <Paper>
-                    <Chart data={this.state.data.results}>
-                        <PieSeries valueField="votes" argumentField="name" />
-                        <Legend/>
-                        <Title text="Election Title"/>
-                        <EventTracker/>
-                        <Tooltip/>
-                        <Animation/>
+                    <Chart data={this.state.data}>
+                        <PieSeries
+                            valueField="votes"
+                            argumentField="candidate"
+                        />
+                        <Legend />
+                        <Title text="UTMSU Vice President Election" />
+                        <EventTracker />
+                        <Tooltip />
+                        <Animation />
                     </Chart>
-                    <Chart data={this.state.data.results} rotated>
-                        <ArgumentAxis/>
-                        <ValueAxis/>
-                        <BarSeries valueField="votes" argumentField="name" />
-                        <Title text="Election Title"/>
-                        <EventTracker/>
-                        <Tooltip/>
-                        <Animation/>
+                    <Chart data={this.state.data} rotated>
+                        <ArgumentAxis />
+                        <ValueAxis />
+                        <BarSeries
+                            valueField="votes"
+                            argumentField="candidate"
+                        />
+                        {/* <Title text="UTMSU Vice President Election"/> */}
+                        <EventTracker />
+                        <Tooltip />
+                        <Animation />
                     </Chart>
                 </Paper>
             </Container>
         );
     }
 }
- 
+
 export default ResultsView;
